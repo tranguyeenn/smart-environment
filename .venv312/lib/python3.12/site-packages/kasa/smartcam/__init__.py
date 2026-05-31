454bb0854c67163c132a5543f5b780d7bfac4f39
Trang Nguyen <tranguyeenn2007@gmail.com>
2026-05-30 21:15:10 -0400

    Remove venv and cache files from tracking

--- .venv312/lib/python3.12/site-packages/kasa/smartcam/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package for supporting tapo-branded cameras."""
-
-from .smartcamchild import SmartCamChild
-from .smartcamdevice import SmartCamDevice
-
-__all__ = ["SmartCamDevice", "SmartCamChild"]
